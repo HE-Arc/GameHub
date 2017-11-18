df23b8b5c7f3ffa327410652713a115a4ae22b29
Gander Laurent <laurent.gander@he-arc.ch>
2017-11-18 16:24:28 +0000

    Apply fixes from StyleCI

--- gamehub.dev/resources/views/layouts/layouttiled.php
+++ gamehub.dev/resources/views/layouts/layouttiled.php
@@ -1,15 +1,15 @@
 
 <div class="col-md-12 col-centered">
-	<?php  
-		for ($i=0; $i < 100; $i++) { 
-	?>
+	<?php 
+        for ($i = 0; $i < 100; $i++) {
+            ?>
 	<div class="col-md-3">
 		<div class="jumbotron col-md-12">
 		  <h2>Game <?php echo $i; ?></h2>
 		  <p>Description</p> 	
 		</div>
 	</div>
-	<?php  
-	}			
-	?>
+	<?php
+        }
+    ?>
 </div>
