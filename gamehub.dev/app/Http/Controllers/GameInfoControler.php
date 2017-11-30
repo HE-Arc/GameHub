0ecd412d39bcaa128c6f80dd15d9252017aa9e9d
Gander Laurent <laurent.gander@he-arc.ch>
2017-11-30 12:08:58 +0000

    Apply fixes from StyleCI

--- gamehub.dev/app/Http/Controllers/GameInfoControler.php
+++ gamehub.dev/app/Http/Controllers/GameInfoControler.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class GameInfoControler extends Controller
 {
-     public function index($steam_id)
+    public function index($steam_id)
     {
-    	return view('infogame');
+        return view('infogame');
     }
 }
--- gamehub.dev/app/Http/Controllers/StartPageControler.php
+++ gamehub.dev/app/Http/Controllers/StartPageControler.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-
-use Illuminate\Http\Request;
 use App\Games;
 
 class StartPageControler extends Controller
 {
     public function index()
     {
-    	$games = Games::all();
+        $games = Games::all();
 
         return view('startpage', ['games' => $games]);
     }
